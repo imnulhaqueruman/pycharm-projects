6b76d25b1a78ec3f4b8b790f616abd7e14276702
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2023-01-16 01:08:02 +0600

    face detection

---
+++ faceDetection.py
@@ -0,0 +1,31 @@
+
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+def detect(gray,frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255, 0), 2)
+    return frame
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame =cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+
+    cv2.imshow('video', canvas)
+    if cv2.waitKey(40) & 0xFF == ord('q'):
+        break
+cap.relase()
+cv2.destroyAllWindows()
+
